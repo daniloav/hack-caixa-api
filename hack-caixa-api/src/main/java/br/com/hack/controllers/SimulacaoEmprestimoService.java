8a85781631c121bc41386c481bbf6a48f407a596
Danilo Oliveira <danilooliveira@MacBook-Air-de-Danilo.local>
2023-06-10 14:49:29 -0300

    funcionou

--- hack-caixa-api/src/main/java/br/com/hack/controllers/SimulacaoEmprestimoService.java
+++ hack-caixa-api/src/main/java/br/com/hack/controllers/SimulacaoEmprestimoService.java
@@ -2,7 +2,6 @@ package br.com.hack.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -12,10 +11,10 @@ import com.azure.messaging.eventhubs.EventHubClientBuilder;
 import com.azure.messaging.eventhubs.EventHubProducerClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import br.com.hack.dao.ProdutoDAO;
-import br.com.hack.model.Parcela;
-import br.com.hack.model.Parcelas;
 import br.com.hack.model.Produto;
 import br.com.hack.model.SimulacaoEmprestimoResponse;
 
@@ -40,15 +39,29 @@ public class SimulacaoEmprestimoService {
 				response.setCodigoProduto(produto.get(i).getCodigoProduto());
 				response.setDescricaoProduto(produto.get(i).getNomeProduto());
 				response.setTaxaJuros(produto.get(i).getTaxaJuros());
-				response.setResultadoSimulacaoSAC(calcularAmortizacaoSAC(valorSolicitado, quantidadeParcelas, response.getTaxaJuros()),
-						calcularAmortizacaoPRICE(valorSolicitado, quantidadeParcelas, response.getTaxaJuros()));
-				// response.setResultadoSimulacaoSAC(
-				// calcularAmortizacaoPrice(valorSolicitado, quantidadeParcelas,
-				// response.getTaxaJuros()));
+				response.setResultadoSimulacao(null);
+				
+				List<SimulacaoEmprestimoResponse.ResultadoSimulacao> resultadoSimulacao = new ArrayList<>();
+				
+		       
+				resultadoSimulacao.add(calcularAmortizacaoSAC(valorSolicitado, quantidadeParcelas, response.getTaxaJuros()));
+		     
+		        resultadoSimulacao.add(calcularAmortizacaoPRICE(valorSolicitado, quantidadeParcelas, response.getTaxaJuros()));
+		        
+				
+		        response.setResultadoSimulacao(resultadoSimulacao);
+		       
 
 				gravarEventoSimulacao(response); // Grava o evento no Eventhub
+				
+				 Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			        String json = gson.toJson(response);
+			        
+			        SimulacaoEmprestimoResponse responsef = gson.fromJson(json, SimulacaoEmprestimoResponse.class);
 
-				return response;
+			        return responsef;
+
+				//return response;
 			}
 
 		}
@@ -56,48 +69,61 @@ public class SimulacaoEmprestimoService {
 		return null; // Retorna null se a simulação falhou ou os dados são inválidos
 	}
 
-	private Parcelas calcularAmortizacaoSAC(double valorSolicitado, int quantidadeParcelas, double taxaJuros) {
-		List<Parcela> parcelas = new ArrayList<Parcela>();
+	private SimulacaoEmprestimoResponse.ResultadoSimulacao calcularAmortizacaoSAC(double valorSolicitado, int quantidadeParcelas, double taxaJuros) {
+		
+		SimulacaoEmprestimoResponse.ResultadoSimulacao sac = new SimulacaoEmprestimoResponse.ResultadoSimulacao();
+        sac.setTipo("SAC");
 
 		double valorAmortizacao = valorSolicitado / quantidadeParcelas;
 		double saldoDevedor = valorSolicitado;
+		
+		List<SimulacaoEmprestimoResponse.Parcela> parcelas = new ArrayList<>();
 
 		for (int i = 1; i <= quantidadeParcelas; i++) {
 			double valorJuros = saldoDevedor * taxaJuros;
 			double valorPrestacao = valorAmortizacao + valorJuros;
-			Parcela parcela = new Parcela();
-			parcela.setNumero(i);
-			parcela.setValorAmortizacao(valorAmortizacao);
-			parcela.setValorJuros(valorJuros);
-			parcela.setValorPrestacao(valorPrestacao);
+			
+			SimulacaoEmprestimoResponse.Parcela parcela = new SimulacaoEmprestimoResponse.Parcela(i, valorAmortizacao, valorJuros, valorPrestacao);;
+			
 			parcelas.add(parcela);
-			saldoDevedor -= valorAmortizacao;
-		}
-		Parcelas parcelaSAC = new Parcelas("SAC", parcelas);
 
-		return parcelaSAC;
+			saldoDevedor -= valorAmortizacao;
+			
+			
+		}sac.setParcelas(parcelas);
+		
+		   for (SimulacaoEmprestimoResponse.Parcela parcela : sac.getParcelas()) {
+		        System.out.println(parcela.getNumero());
+		    }
+		
+		return sac;
 	}
 
-	private Parcelas calcularAmortizacaoPRICE(double valorSolicitado, int quantidadeParcelas, double taxaJuros) {
+	private SimulacaoEmprestimoResponse.ResultadoSimulacao calcularAmortizacaoPRICE(double valorSolicitado, int quantidadeParcelas, double taxaJuros) {
 
-		List<Parcela> parcelas = new ArrayList<Parcela>();
+		SimulacaoEmprestimoResponse.ResultadoSimulacao price = new SimulacaoEmprestimoResponse.ResultadoSimulacao();
+        price.setTipo("PRICE");
 
 		double valorParcela = calcularValorParcelaPrice(valorSolicitado, taxaJuros, quantidadeParcelas);
 		double saldoDevedor = valorSolicitado;
+		
+		List<SimulacaoEmprestimoResponse.Parcela> parcelas = new ArrayList<>();
 
 		for (int j = 1; j <= quantidadeParcelas; j++) {
 			double juros = saldoDevedor * taxaJuros;
 			double amortizacao = valorParcela - juros;
-			Parcela parcela = new Parcela();
-			parcela.setNumero(j);
-			parcela.setValorAmortizacao(amortizacao);
-			parcela.setValorJuros(juros);
-			parcela.setValorPrestacao(valorParcela);
+			
+			SimulacaoEmprestimoResponse.Parcela parcela = new SimulacaoEmprestimoResponse.Parcela(j, amortizacao,juros,valorParcela);
+			
 			parcelas.add(parcela);
+			
 			saldoDevedor -= amortizacao;
-		}
-		Parcelas parcelaPRICE = new Parcelas("PRICE", parcelas);
-		return parcelaPRICE;
+			
+			
+		
+		}price.setParcelas(parcelas);
+
+		return price;
 	}
 
 	private double calcularValorParcelaPrice(double valorSolicitado, double taxaJuros, int quantidadeParcelas) {
